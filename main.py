25b4dd09b97666d435e409ab3ca27f3a39035713
Alexander Filippov <sash.f@rambler.ru>
2022-09-16 17:51:34 +0300

    Decorators3

--- main.py
+++ main.py
@@ -11,30 +11,38 @@ def write_logger(func):
         file = open("logger.txt", "w", encoding='utf-8')
         file.write(f'Дата и время вызова функции {func.__name__}: ')
         file.write(str('{}'.format(start)))
-        file.write(f'\nФункция {str(func.__name__)} вызвана с параметрами:{str(*args)} {str(dict(**kwargs))}')
+        file.write(f'\nФункция {str(func.__name__)} вызвана с параметрами:{str(*args)} {dict(**kwargs)}')
         file.write(f'\nФункция {str(func.__name__)} возвращает значение температуры: {str(func(*args, **kwargs))} градусов цельсия')
+        result = func(*args, **kwargs)
         file.close()
+        return f'Температура в городе {str(*args)}{("".join(dict(**kwargs).values()))} {result} градусов'
     return wrapper
 
 # Задание №2
 
-def patch_logger(func):
-    def wrapper(*args, **kwargs):
-        start = datetime.datetime.now()
-        func(*args, **kwargs)
-        log_name = "logger.txt"
-        file = open(log_name, "w", encoding='utf-8')
-        file.write(f'Дата и время вызова функции {func.__name__}: ')
-        file.write(str('{}'.format(start)))
-        file.write(f'\nФункция {str(func.__name__)} вызвана с параметрами: {str(*args)} {str(dict(**kwargs))}')
-        file.write(f'\nФункция {str(func.__name__)} возвращает значение температуры: {str(func(*args, **kwargs))} градусов цельсия')
-        base_patch = os.getcwd()
-        file.write(f'\nПуть к логам {os.path.join(base_patch, log_name)}')
-        file.close()
-    return wrapper
+def parametrized_decor(parametr):
+    def patch_logger(func):
+        def wrapper(*args, **kwargs):
+            start = datetime.datetime.now()
+            func(*args, **kwargs)
+            file = open(log_name, "w", encoding='utf-8')
+            file.write(f'Дата и время вызова функции {func.__name__}: ')
+            file.write(str('{}'.format(start)))
+            file.write(f'\nФункция {str(func.__name__)} вызвана с параметрами: {str(*args)} {dict(**kwargs)}')
+            file.write(f'\nФункция {str(func.__name__)} возвращает значение температуры: {str(func(*args, **kwargs))} градусов цельсия')
+            result = func(*args, **kwargs)
+            file.close()
+            return f'Температура в городе {str(*args)}{("".join(dict(**kwargs).values()))} {result} градусов\n' \
+                   f'Путь сохраненного файла логирования {parametr}'
+        return wrapper
+    return patch_logger
+
+log_name = "logger.txt"
+base_patch = os.getcwd()
+path = os.path.join(base_patch, log_name)
 
 # @write_logger
-@patch_logger
+@parametrized_decor(parametr = path)
 def temperature_air(town):
     try:
         gismeteo = Gismeteo()
@@ -45,6 +53,7 @@ def temperature_air(town):
     except:
         print('Некорректные данные!')
 
+print(temperature_air(town="Москва"))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
